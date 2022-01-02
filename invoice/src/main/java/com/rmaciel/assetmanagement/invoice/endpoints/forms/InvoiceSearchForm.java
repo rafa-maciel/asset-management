1003f773c5329ce4f94088f28619880e4d0e977e
Rafa Maciel <rafael.macielf@gmail.com>
2022-01-02 09:16:12 -0300

    fixed date format from JSON fields receivers

--- invoice/src/main/java/com/rmaciel/assetmanagement/invoice/endpoints/forms/InvoiceSearchForm.java
+++ invoice/src/main/java/com/rmaciel/assetmanagement/invoice/endpoints/forms/InvoiceSearchForm.java
@@ -1,10 +1,12 @@
 package com.rmaciel.assetmanagement.invoice.endpoints.forms;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.rmaciel.academy.core.models.Invoice;
 import com.rmaciel.academy.core.specifications.utils.DateSearchType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -16,11 +18,25 @@ public class InvoiceSearchForm {
     private Long number;
     private String vendor;
     private String vendorCNPJ;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate startsAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate startAtMax;
+
     private DateSearchType startsAtSearchType = DateSearchType.EQUAL;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate endsAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate endsAtMax;
+
     private DateSearchType endsAtSearchType = DateSearchType.EQUAL;
 
     public Specification<Invoice> buildSpecs() {
