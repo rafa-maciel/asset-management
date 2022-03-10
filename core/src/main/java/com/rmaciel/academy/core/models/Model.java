9131f3d37815f3ef0b1f67cd0f10c44ff1a6227f
Rafael Maciel <rafael.macielf@gmail.com>
2022-03-10 14:06:39 -0300

    fields attributes has been refactored

--- core/src/main/java/com/rmaciel/academy/core/models/Model.java
+++ core/src/main/java/com/rmaciel/academy/core/models/Model.java
@@ -3,10 +3,7 @@ package com.rmaciel.academy.core.models;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -21,12 +18,22 @@ public class Model {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotEmpty @NotNull @Length(min = 2, max = 30) @NonNull
+    @NotEmpty
+    @NonNull
+    @NotNull
+    @Length(min = 2, max = 30)
+    @Column(unique = true)
     private String title;
 
-    @NotEmpty @NotNull @Length(min = 2, max = 30) @NonNull
+    @NotEmpty
+    @NotNull
+    @Length(min = 2, max = 30)
+    @NonNull
     private String brand;
 
-    @NotEmpty @NotNull @Length(min = 2, max = 30) @NonNull
+    @NotEmpty
+    @NotNull
+    @Length(min = 2, max = 50)
+    @NonNull
     private String type;
 }
