1a7cb35f2b9775604db5ab0dac0362ebc420ace4
Rafa Maciel <rafael.macielf@gmail.com>
2022-03-12 19:44:05 -0300

    handle custom exceptions errors

--- location/src/main/java/com/rmaciel/assetmanagement/location/endpoints/CustomExceptionHandle.java
+++ location/src/main/java/com/rmaciel/assetmanagement/location/endpoints/CustomExceptionHandle.java
@@ -1,2 +1,32 @@
-package com.rmaciel.assetmanagement.location.endpoints;public class CustomExceptionHandle {
+package com.rmaciel.assetmanagement.location.endpoints;
+
+import com.rmaciel.assetmanagement.location.utils.ApiErrorResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class CustomExceptionHandle extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+        }
+        for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+            errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+        }
+
+        ApiErrorResponse errorResponse = new ApiErrorResponse(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors);
+        return handleExceptionInternal(ex, errorResponse, headers, errorResponse.getStatus(), request);
+    }
 }
